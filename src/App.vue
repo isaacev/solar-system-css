8a4aebcf30057c8139f70a7d81b63ca18187d56d
isaacev <isaacevavold@gmail.com>
2017-12-13 01:48:28 -0600

    remove output channels for now

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,9 @@
   <div id="app">
     <header>
       <h1>solar-system.css</h1>
-      <div id="dynamic-solar-system" v-html="output.cache.markup.content"></div>
     </header>
     <main>
       <row v-for="(b, i) in bodies" key="i" v-on:remove="remove(i)" :body="b"></row>
-      <div class="outputs">
-        <output-channel
-          v-for="(c, j) in output.channels"
-          key="j"
-          :label="c.label"
-          :output="getOutput(c.name)"></output-channel>
       </div>
     </main>
   </div>
@@ -19,7 +12,6 @@
 
 <script>
   import Row from './Row'
-  import OutputChannel from './OutputChannel'
   import convert from './convert'
   import { debounce, addSystemStyles } from './util'
 
@@ -27,7 +19,6 @@
     name: 'app',
     components: {
       Row,
-      OutputChannel
     },
 
     data () {
@@ -54,117 +45,24 @@
             orbit: 300,
             speed: 100
           }
-        ],
-        output: {
-          cache: {
-            stylesheet: {
-              pending: true,
-              content: ''
-            },
-            markup: {
-              pending: true,
-              content: ''
-            }
-          },
-          channels: [
-            {
-              label: 'Stylesheet',
-              name: 'stylesheet'
-            },
-            {
-              label: 'Markup',
-              name: 'markup'
-            }
-          ]
-        }
-      }
-    },
-    watch: {
-      bodies: {
-        deep: true,
-        handler: debounce(function (newVal, oldVal) {
-          this.recompile()
-        }, 500)
+        ]
       }
     },
     methods: {
       remove: function (i) {
         this.bodies.splice(i, 1)
-      },
-      toSCSS: function (bodies, cb) {
-        const scss = convert.toSCSS('system', 400, bodies)
-        window.sass.compile(scss, (result) => {
-          if (result.status === 0) {
-            cb(null, result.text)
-          } else {
-            cb(result.formatted, '')
-          }
-        })
-        return ''
-      },
-      toHTML: function (bodies, cb) {
-        cb(null, convert.toHTML('system', bodies))
-      },
-      updateStylesheet: function () {
-        const bodies = this.bodies.slice()
-        bodies.unshift({
-          name: 'sun',
-          texture: 'yellow',
-          size: 96,
-          orbit: 0,
-          speed: 0,
-          satellites: this.bodies
-        })
-        this.output.cache.stylesheet.pending = true
-        this.toSCSS(bodies, (err, css) => {
-          this.writeCache('stylesheet', css)
-          addSystemStyles(css)
-          this.output.cache.stylesheet.pending = false
-        })
-      },
-      updateMarkup: function () {
-        const bodies = [{
-          name: 'sun',
-          texture: 'yellow',
-          size: 96,
-          orbit: 0,
-          speed: 0,
-          satellites: this.bodies
-        }]
-        this.output.cache.markup.pending = true
-        this.toHTML(bodies, (err, html) => {
-          this.writeCache('markup', html)
-          this.output.cache.markup.pending = false
-        })
-      },
-      recompile: function () {
-        console.log('recompile')
-        this.updateStylesheet()
-        this.updateMarkup()
-      },
-      readCache: function (channel) {
-        if (this.output.cache[channel].pending) {
-          return ''
-        } else if (this.output.cache[channel].content) {
-          return this.output.cache[channel].content
-        } else {
-          console.log(this.output.cache[channel])
-          throw 'cache miss'
-        }
-      },
-      writeCache: function (channel, newVal) {
-        this.output.cache[channel].content = newVal
-      },
-      getOutput: function (channel) {
-        return this.readCache(channel)
       }
     }
   }
 </script>
 
 <style lang="scss">
+  html, body {
+    background: #edefed;
+  }
+
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'Space Mono', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
